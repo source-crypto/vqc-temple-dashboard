8f2c4b06906b50cd69d6ebc5389cdf044f507f99
Leap <git@leap.new>
2025-10-22 03:29:03 +0000

    Manual code change

--- backend/blockchain/contracts.ts
+++ backend/blockchain/contracts.ts
@@ -478,7 +478,7 @@ function generateContractAddress(): string {
 }
 
 function isValidAddress(address: string): boolean {
-  return /^0x[a-fA-F0-9]{40}$/.test(address);
+} return /^0x[a-fA-F0-9]{40}$/.test(address);
 // quantumTemporalContracts.ts
 import { api, APIError } from "encore.dev/api";
 import { monitoredBlockchainDB as blockchainDB } from "./db";
